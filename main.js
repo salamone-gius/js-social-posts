8b58063f901b59a07ea52134536df1fbbf144ea0
salamone-gius <salamone.gius@gmail.com>
2022-05-16 15:18:30 +0200

    posts printed

--- main.js
+++ main.js
@@ -58,7 +58,50 @@ const posts = [
 
 // Descrizione
 // Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
-// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed:
+// - aggancio l'elemento HTML su cui andrò a stampare i post;
+const postsContainer = document.querySelector(".posts-list");
+// - scorrendo tutto l'array posts con ciclo definito...
+for (let i = 0; i < posts.length; i++) {
+    // - aggiungo dinamicamente ogni elemento (post) dell'array posts all'HTML;
+    postsContainer.innerHTML += 
+    // - importo TUTTA la struttura HTML dei post;
+    // - inserisco il VALORE della relativa CHIAVE richiamandolo dall'array posts con template literal;
+        `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `
+    ;
+}
+
 // Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 // Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 // BONUS
